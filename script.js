761889ae140cf7d64a46e4822ce5682cce0e5a28
Glitch (hello-webpage) <none>
2021-03-18 06:23:32 +0000

    🍨🍖 Checkpoint ./script.js:29871205/256 ./index.html:29871205/130

--- script.js
+++ script.js
@@ -8,7 +8,13 @@ function startGame()
   //initialize game variable
   progress = 0;
   gamePlaying = true;
-  document.getElementById("startBtn").classList.add("hidden");
+  document.getElementById("startBtn").classList.add("hidden"); //we are adding the class .hidden to id="startBtn"
   document.getElementById("stopBtn").classList.remove("hidden");
   //document means the DOM as a js object
+}
+function stopGame()
+{
+  gamePlaying = false;
+  document.getElementId("startBtn").classList.remove("hidden");
+  document.getElementId("stopBtn").classList.add("hidden");
 }
